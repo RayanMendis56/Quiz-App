e1531ef52134387ec24122099e7f9ea217b589eb
RayanMendis56 <rayanmendisa13@gmail.com>
2026-09-03 03:42:17 +0530

    feat(backend): add Question entity, repository, service, and GET endpoints

---
+++ backend/src/main/java/com/rayan/controller/QuestionController.java
@@ -0,0 +1,31 @@
+package com.rayan.controller;
+
+import com.rayan.Question;
+import com.rayan.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+
+    }
+
+}
---
+++ backend/src/main/java/com/rayan/repository/QuestionRepository.java
@@ -0,0 +1,13 @@
+package com.rayan.repository;
+
+import com.rayan.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+}
