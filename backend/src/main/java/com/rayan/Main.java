14b2f42bec0f189310839d9dc511bdaf4ccb1e97
RayanMendis56 <rayanmendisa13@gmail.com>
2026-09-02 23:54:04 +0530

    initial commit

---
+++ backend/src/main/java/com/rayan/Main.java
@@ -0,0 +1,10 @@
+package com.rayan;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main() {
+
+        }
+    }
+
